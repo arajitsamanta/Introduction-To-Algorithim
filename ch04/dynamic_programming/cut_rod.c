29d5361722142f8b3032a39d56ed9ec71f6f8a1d
Arajit Samanta <arijit24@gmail.com>
2018-10-21 22:47:13 -0500

    added time calculation

--- ch04/dynamic_programming/cut_rod.c
+++ ch04/dynamic_programming/cut_rod.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include<limits.h>
+#include<time.h>
+
+#define MIN_INT (-1 << sizeof(int)) -1;
 
 int max(int a, int b)
 {
@@ -19,7 +23,7 @@ int cut_rod(int p[], int n)
     return 0;
   }
 
-  int max_int = -9999999;
+  int max_int = MIN_INT;
   for (int i = 1; i <= n; i++)
   {
     max_int = max(max_int, p[i] + cut_rod(p, n - i));
@@ -30,7 +34,12 @@ int cut_rod(int p[], int n)
 int main()
 {
   //length 0,1,2....n, Prices are of p[i]. when length =0, price is 0, when lentgth is 1 price is 1, when lentgth is 2 price is 5 and so on.
-  int prices[] = {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+  int prices[] = {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30,1, 5, 8, 9, 10, 17, 17, 20, 24, 30,1, 5, 8, 9, 10, 17, 17, 20, 24, 30,1, 5, 8, 9, 10, 17, 17, 20, 24, 30,1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
 
-  printf("Max revenue %d ", cut_rod(prices, 4));
+  clock_t begin = clock();
+  printf("Max revenue %d ", cut_rod(prices, 32));
+  clock_t end = clock();
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("Time taken: %f",time_spent);
+  return 0;
 }
