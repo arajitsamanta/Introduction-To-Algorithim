77d32f508c8933db1942a9a1cc9600409534bc86
Samanta <Arajit.Samanta@ehi.com>
2018-12-21 15:53:25 -0600

    matrix chain multiplication work in progress

--- ch04/dynamic_programming/matrix_chain_multiplication.c
+++ ch04/dynamic_programming/matrix_chain_multiplication.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <limits.h>
+#include <time.h>
+#include<stdlib.h>
 
-/* Macro definition */
-#define MAX_INT 1<<(8*sizeof(int) - 1);
+/*
+Matrix Mi has dimension p[i-1] x p[i] for i = 1..n
 
-// Matrix Ai has dimension p[i-1] x p[i] for i = 1..n
+Complexity Analysis
+===================
+Exponential time.
+
+Time: 2^n
+Space: No extra space apart from stack activation frame.
+*/
 int matrix_chain_order(int p[], int i, int j)
 {
     if (i == j)
@@ -22,7 +30,6 @@ int matrix_chain_order(int p[], int i, int j)
         count = matrix_chain_order(p, i, k) +
                 matrix_chain_order(p, k + 1, j) +
                 p[i - 1] * p[k] * p[j];
-
         if (count < min)
             min = count;
     }
@@ -31,13 +38,84 @@ int matrix_chain_order(int p[], int i, int j)
     return min;
 }
 
+/*
+Complexity Analysis
+===================
+Time: O(n^3)
+Auxiliary Space: O(n^2)
+*/
+void matrix_chain_order_memoized(int p[], int n, int **m, int **s)
+{
+    /* For simplicity of the program, one extra row and one 
+       extra column are allocated in m[][].  0th row and 0th 
+       column of m[][] are not used */
+    //int m[n][n];
+
+    /* s records which index of k achieved the optimal cost in computing
+        m[i,j]. We shall use the table s to construct an optimal solution. */
+    //int s[n-1][n-1];
+
+    int i, j, k, L, q;
+
+    /* m[i,j] = Minimum number of scalar multiplications needed 
+       to compute the matrix A[i]A[i+1]...A[j] = A[i..j] where 
+       dimension of A[i] is p[i-1] x p[i] */
+
+    // cost is zero when multiplying one matrix.
+    for (i = 1; i < n; i++)
+        m[i][i] = 0;
+
+    // L is chain length.
+    for (L = 2; L < n; L++)
+    {
+        for (i = 1; i < n - L + 1; i++)
+        {
+            j = i + L - 1;
+            m[i][j] = INT_MAX;
+            for (k = i; k <= j - 1; k++)
+            {
+                // q = cost/scalar multiplications
+                q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+                if (q < m[i][j])
+                    m[i][j] = q;
+                s[i][j] = k;
+            }
+        }
+    }
+
+    //return m[1][n - 1];
+}
+
 int main()
 {
-    int arr[] = {1, 2, 3, 4, 3};
-    int max1=INT_MAX;
-    int max2=MAX_INT;
-     printf("size int %d max1 %d max2 %d %d \n", sizeof(int),  max1 , ((1<<30)) );
-    int n = sizeof(arr) / sizeof(arr[0]);
-    printf("Minimum number of multiplications is %d ", matrix_chain_order(arr, 1, n - 1));
+    // Matrix is M1=30*35, M2=35*15 M3=15*5 M4=5*10 M5=10*20 M6=20*25
+    int arr[] = {30, 35, 15, 5, 10, 20, 25};
+    int i, n = sizeof(arr) / sizeof(arr[0]);
+    int **m, **s;
+
+    clock_t begin = clock();
+    printf("Minimum number of multiplications(Recursive): %d\n", matrix_chain_order(arr, 1, n - 1));
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Time taken: %f\n", time_spent);
+
+    /* allocate the rows */
+    m = malloc(n * sizeof(int *));
+    s = malloc((n - 1) * sizeof(int *));
+
+    /* allocate columns */
+    for (i = 0; i < n; i++)
+    {
+        m[i] = malloc(n * sizeof(*m[i]));
+        s[i] = malloc((n - 1) * sizeof(*s[i]));
+    }
+
+    begin = clock();
+    matrix_chain_order_memoized(arr, n, m, s);
+    printf("Minimum number of multiplications(Memoized): %d\n", m[1][n - 1]);
+    end = clock();
+    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Time taken: %f", time_spent);
+
     return 0;
 }
